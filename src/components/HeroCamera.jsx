bb617c5798cec19ec226fb38def5faf1cbbf3c64
Amira Boubaker <amiraboubakeresprims@gmail.com>
2024-11-14 20:46:18 +0100

    added Button contact in Hero

--- src/components/HeroCamera.jsx
+++ src/components/HeroCamera.jsx
@@ -8,18 +8,18 @@ const HeroCamera = ({ children, isMobile }) => {
     useFrame((state, delta) => {
         easing.damp3(state.camera.position, [0, 0, 20], 0.25, delta);
 
-        if (!isMobile) {
+        //if (!isMobile) {
             easing.dampE(
                 groupRef.current.rotation,
                 [-state.pointer.y / 3, -state.pointer.x / 5, 0],
                 0.25,
                 delta
             );
-        }
+        //}
     });
 
     return (
-        <group ref={groupRef}>
+        <group ref={groupRef} scale={ isMobile ? 1: 1.3}>
             {children}
         </group>
     );
